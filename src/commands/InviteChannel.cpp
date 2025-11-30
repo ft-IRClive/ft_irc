fa6102e69ba8d3edd4f6cd82731172b2aebe70cf
loreeue <loretouzquiano@gmail.com>
2025-11-30 18:57:00 +0100

    El modo operador e invitación arreglados

--- src/commands/InviteChannel.cpp
+++ src/commands/InviteChannel.cpp
@@ -6,7 +6,7 @@
 /*   By: loruzqui <loruzqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 15:15:41 by loruzqui          #+#    #+#             */
-/*   Updated: 2025/11/29 15:42:11 by loruzqui         ###   ########.fr       */
+/*   Updated: 2025/11/30 18:18:41 by loruzqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,57 +15,67 @@
 
 void Server::_handlerClientInvite(const std::string &buffer, const int fd)
 {
-	Client*						client = _getClient(fd);
-	std::vector<std::string>	params = _splitBuffer(buffer, SPACE);
+	Client	*client = _getClient(fd);
 
-	if (params.size() < 2)
+	if (!client || !client->getIsLogged())
 	{
-		_sendResponse(fd, ERR_MISSINGPARAMS(_getHostname(), client->getNname()));
-		_replyCode = 461;
+		_sendResponse(fd, ERR_NOTREGISTERED(_getHostname(), "*"));
+		return;
 	}
-	else if (!client->getIsLogged())
-	{
-		_sendResponse(fd, ERR_NOTREGISTERED(_getHostname(), client->getNname()));
-		_replyCode = 451;
-	}
-	else
-	{
-		std::string	targetNName = params[0];
-		std::string	targetChannel = params[1];
 
-		Client*		invitedClient = _getClient(targetChannel);
-		Channel*	channel = _getChannel(targetChannel);
+	std::istringstream iss(buffer);
+	std::string targetNickname, channelName;
 
-		if (!channel)
-		{
-			_sendResponse(fd, ERR_NOSUCHCHANNEL(_getHostname(), targetChannel));
-			_replyCode = 403;
-		}
-		else if (!channel->hasClient(client))
-		{
-			_sendResponse(fd, ERR_NOTONCHANNEL(_getHostname(), targetChannel));
-			_replyCode = 442;
-		}
-		else if (!channel->isChannelOperator(client->getNname()))
-		{
-			_sendResponse(fd, ERR_NOPRIVILEGES(_getHostname(), client->getNname()));
-			_replyCode = 481;
-		}
-		else if (!invitedClient)
-		{
-			_sendResponse(fd, ERR_NONICKNAME(_getHostname(), targetNName));
-			_replyCode = 401;
-		}
-		else if (channel->hasClient(invitedClient))
-		{
-			_sendResponse(fd, ERR_USERONCHANNEL(_getHostname(), targetNName, targetChannel));
-			_replyCode = 443;
-		}
-		else
-		{
-			invitedClient->addChannelInvited(targetChannel);
-			_sendResponse(fd, RPL_INVITING(client->getHostName(), targetChannel, client->getNname(), targetNName));
-			_replyCode = 200;
-		}
+	iss >> targetNickname >> channelName;
+
+	if (targetNickname.empty() || channelName.empty())
+	{
+		_sendResponse(fd, ERR_MISSINGPARAMS(_getHostname(), "INVITE"));
+		return;
 	}
+
+	Channel *channel = _getChannel(channelName);
+	if (!channel)
+	{
+		_sendResponse(fd, ERR_NOSUCHCHANNEL(_getHostname(), channelName));
+		return;
+	}
+
+	if (!channel->hasClient(client))
+	{
+		_sendResponse(fd, ERR_NOTONCHANNEL(_getHostname(), channelName));
+		return;
+	}
+
+	if (!channel->isChannelOperator(client->getNname()))
+	{
+		_sendResponse(fd, ERR_CHANOPRIVSNEEDED(_getHostname(), channelName));
+		return;
+	}
+
+	Client	*invitedClient = _getClient(targetNickname);
+	if (!invitedClient)
+	{
+		_sendResponse(fd, ERR_NOSUCHNICK(_getHostname(), targetNickname));
+		return;
+	}
+
+	if (channel->hasClient(invitedClient))
+	{
+		_sendResponse(fd, ERR_USERONCHANNEL(_getHostname(), targetNickname, channelName));
+		return;
+	}
+
+	invitedClient->addChannelInvited(channelName);
+	std::string inviteConfirm = RPL_INVITING(
+		client->getHostName(),
+		channelName,
+		client->getNname(),
+		targetNickname
+	);
+	_sendResponse(fd, inviteConfirm);
+
+	std::string inviteMsg = ":" + client->getNname() + "!" + client->getHostName() +
+						" INVITE " + targetNickname + " " + channelName + CRLF;
+	_sendResponse(invitedClient->getFd(), inviteMsg);
 }
