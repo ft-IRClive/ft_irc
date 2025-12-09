331905e883e7fdbbc1ed47da4fb73086f9b8be5d
loreeue <loretouzquiano@gmail.com>
2025-12-09 17:49:52 +0100

    Tabs

--- src/commands/Kick.cpp
+++ src/commands/Kick.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Kick.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: claudia <claudia@student.42.fr>            +#+  +:+       +#+        */
+/*   By: loruzqui <loruzqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 15:16:08 by loruzqui          #+#    #+#             */
-/*   Updated: 2025/12/02 12:06:19 by claudia          ###   ########.fr       */
+/*   Updated: 2025/12/09 17:48:46 by loruzqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,75 +14,75 @@
 
 void Server::_handlerClientKick(const std::string &buffer, const int fd)
 {
-    Client *client = _getClient(fd);
-    if (!client || !client->getIsLogged())
-    {
-        std::string nick = (client ? client->getNname() : "*");
-        _sendResponse(fd, ERR_NOTREGISTERED(_getHostname(), nick));
-        return;
-    }
+	Client	*client = _getClient(fd);
+	if (!client || !client->getIsLogged())
+	{
+		std::string nick = (client ? client->getNname() : "*");
+		_sendResponse(fd, ERR_NOTREGISTERED(_getHostname(), nick));
+		return;
+	}
 
-    std::istringstream iss(buffer);
-    std::string prefix, command, channelName, targetNickname, reason;
-    if (!buffer.empty() && buffer[0] == ':')
-        iss >> prefix;
+	std::istringstream	iss(buffer);
+	std::string			prefix, command, channelName, targetNickname, reason;
+	if (!buffer.empty() && buffer[0] == ':')
+		iss >> prefix;
 
-    iss >> command;
-    iss >> channelName >> targetNickname;
+	iss >> command;
+	iss >> channelName >> targetNickname;
 
-    if (!targetNickname.empty() && targetNickname[0] == ':')
-        targetNickname.erase(0, 1);
+	if (!targetNickname.empty() && targetNickname[0] == ':')
+		targetNickname.erase(0, 1);
 
-    std::getline(iss >> std::ws, reason);
-    if (!reason.empty() && reason[0] == ':')
-        reason = reason.substr(1);
-    if (reason.empty())
-        reason = "No reason given";
+	std::getline(iss >> std::ws, reason);
+	if (!reason.empty() && reason[0] == ':')
+		reason = reason.substr(1);
+	if (reason.empty())
+		reason = "No reason given";
 
-    if (channelName.empty() || targetNickname.empty())
-    {
-        _sendResponse(fd, ERR_MISSINGPARAMS(_getHostname(), "KICK"));
-        return;
-    }
+	if (channelName.empty() || targetNickname.empty())
+	{
+		_sendResponse(fd, ERR_MISSINGPARAMS(_getHostname(), "KICK"));
+		return;
+	}
 
-    Channel *channel = _getChannel(channelName);
-    if (!channel)
-    {
-        _sendResponse(fd, ERR_NOSUCHCHANNEL(_getHostname(), channelName));
-        return;
-    }
+	Channel	*channel = _getChannel(channelName);
+	if (!channel)
+	{
+		_sendResponse(fd, ERR_NOSUCHCHANNEL(_getHostname(), channelName));
+		return;
+	}
 
-    if (!channel->hasClient(client))
-    {
-        _sendResponse(fd, ERR_NOTONCHANNEL(_getHostname(), channelName));
-        return;
-    }
-    if (!channel->isChannelOperator(client->getNname()))
-    {
+	if (!channel->hasClient(client))
+	{
+		_sendResponse(fd, ERR_NOTONCHANNEL(_getHostname(), channelName));
+		return;
+	}
+	if (!channel->isChannelOperator(client->getNname()))
+	{
 		_sendResponse(fd, ERR_CHANOPRIVSNEEDED(_getHostname(), client->getNname(), channelName));
-        return;
-    }
-    Client *targetClient = _getClient(targetNickname);
-    if (!targetClient)
-    {
-        _sendResponse(fd, ERR_NOSUCHNICK(_getHostname(), targetNickname));
-        return;
-    }
+		return;
+	}
+	Client	*targetClient = _getClient(targetNickname);
+	if (!targetClient)
+	{
+		_sendResponse(fd, ERR_NOSUCHNICK(_getHostname(), targetNickname));
+		return;
+	}
 
-    if (!channel->hasClient(targetClient))
-    {
-        _sendResponse(fd, ERR_USERNOTINCHANNEL(_getHostname(), targetNickname, channelName));
-        return;
-    }
+	if (!channel->hasClient(targetClient))
+	{
+		_sendResponse(fd, ERR_USERNOTINCHANNEL(_getHostname(), targetNickname, channelName));
+		return;
+	}
 
-    std::string kickMsg = RPL_KICK(
-        client->getHostName(),
-        channelName,
-        client->getNname(),
-        targetClient->getNname(),
-        reason
-    );
+	std::string	kickMsg = RPL_KICK(
+		client->getHostName(),
+		channelName,
+		client->getNname(),
+		targetClient->getNname(),
+		reason
+	);
 
-    _broadcastToChannel(channelName, kickMsg, -1);
-    channel->kick(targetClient);
+	_broadcastToChannel(channelName, kickMsg, -1);
+	channel->kick(targetClient);
 }
--- src/commands/Part.cpp
+++ src/commands/Part.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Part.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: claudia <claudia@student.42.fr>            +#+  +:+       +#+        */
+/*   By: loruzqui <loruzqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 15:17:18 by loruzqui          #+#    #+#             */
-/*   Updated: 2025/12/03 12:12:38 by claudia          ###   ########.fr       */
+/*   Updated: 2025/12/09 17:49:21 by loruzqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,64 +14,63 @@
 
 void Server::_handlerClientPart(const std::string &parameters, const int fd)
 {
-    Client* client = _getClient(fd);
-    std::string server = _getHostname();
-    std::string nick = client ? client->getNname() : "";
+	Client*		client = _getClient(fd);
+	std::string	server = _getHostname();
+	std::string	nick = client ? client->getNname() : "";
 
-    if (!client || !client->getIsLogged())
-    {
-        _sendResponse(fd, ERR_NOTREGISTERED(server, nick));
-        _replyCode = 451;
-        return;
-    }
+	if (!client || !client->getIsLogged())
+	{
+		_sendResponse(fd, ERR_NOTREGISTERED(server, nick));
+		_replyCode = 451;
+		return;
+	}
 
-    if (parameters.empty())
-    {
-        _sendResponse(fd, ERR_MISSINGPARAMS(server, nick));
-        _replyCode = 461;
-        return;
-    }
+	if (parameters.empty())
+	{
+		_sendResponse(fd, ERR_MISSINGPARAMS(server, nick));
+		_replyCode = 461;
+		return;
+	}
 
-    std::string channelName;
-    std::string partMsg;
-    std::istringstream iss(parameters);
+	std::string			channelName;
+	std::string			partMsg;
+	std::istringstream	iss(parameters);
 
-    iss >> channelName;                 
-    std::getline(iss, partMsg);        
+	iss >> channelName;
+	std::getline(iss, partMsg);
 
-    if (!partMsg.empty() && partMsg[0] == ' ')
-        partMsg.erase(0, 1);
-    if (!partMsg.empty() && partMsg[0] == ':')
-        partMsg.erase(0, 1);
+	if (!partMsg.empty() && partMsg[0] == ' ')
+		partMsg.erase(0, 1);
+	if (!partMsg.empty() && partMsg[0] == ':')
+		partMsg.erase(0, 1);
 
-    Channel* channel = _getChannel(channelName);
+	Channel*	channel = _getChannel(channelName);
 
-    if (!channel)
-    {
-        _sendResponse(fd, ERR_NOSUCHCHANNEL(server, channelName));
-        _replyCode = 403;
-        return;
-    }
+	if (!channel)
+	{
+		_sendResponse(fd, ERR_NOSUCHCHANNEL(server, channelName));
+		_replyCode = 403;
+		return;
+	}
 
-    if (!channel->hasClient(client))
-    {
-        _sendResponse(fd, ERR_NOTONCHANNEL(server, channelName));
-        _replyCode = 442;
-        return;
-    }
+	if (!channel->hasClient(client))
+	{
+		_sendResponse(fd, ERR_NOTONCHANNEL(server, channelName));
+		_replyCode = 442;
+		return;
+	}
 
-    std::string msg = ":" + client->getNname() + "!" +
-                      client->getUname() + "@" +
-                      client->getHostName() +
-                      " PART " + channelName;
+	std::string	msg = ":" + client->getNname() + "!" +
+					client->getUname() + "@" +
+					client->getHostName() +
+					" PART " + channelName;
 
-    if (!partMsg.empty())
-        msg += " :" + partMsg;
+	if (!partMsg.empty())
+		msg += " :" + partMsg;
 
-    msg += CRLF;
+	msg += CRLF;
 
-    _broadcastToChannel(channelName, msg, -1);
-    channel->part(client);
-    _replyCode = 200;
+	_broadcastToChannel(channelName, msg, -1);
+	channel->part(client);
+	_replyCode = 200;
 }
-
