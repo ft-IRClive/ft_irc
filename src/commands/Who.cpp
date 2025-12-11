5d4af3ba46c64bc1e0c5458232127acd108ee457
Claudia <sofiagil2288@gmail.com>
2025-12-11 12:34:51 +0100

    funciona who, creo

--- src/commands/Who.cpp
+++ src/commands/Who.cpp
@@ -3,50 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   Who.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: loruzqui <loruzqui@student.42.fr>          +#+  +:+       +#+        */
+/*   By: claudia <claudia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 15:29:14 by loruzqui          #+#    #+#             */
-/*   Updated: 2025/12/10 17:53:21 by loruzqui         ###   ########.fr       */
+/*   Updated: 2025/12/11 11:59:20 by claudia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/Server.hpp"
 
-void	Server::_handlerClientWho(const std::string &buffer, const int fd)
+void Server::_handlerClientWho(const std::string &params, const int fd)
 {
-	Client	*client = _getClient(fd);
-	if (!client)
-		return;
+    Client *client = _getClient(fd);
+    if (!client)
+        return;
+    if (params.empty())
+    {
+        _sendResponse(fd, ERR_MISSINGPARAMS(_hostname, client->getNname()));
+        return;
+    }
+    Channel *channel = _getChannel(params);
+    if (!channel)
+    {
+        _sendResponse(fd, ERR_NOSUCHCHANNEL2(_hostname, client->getNname(), params));
+        return;
+    }
+    const std::vector<Client*> &clients = channel->getChClients();
+    for (std::vector<Client*>::const_iterator it = clients.begin(); it != clients.end(); ++it)
+    {
+        Client *c = *it;
+        if (!c) 
+            continue;
 
-	std::vector<std::string>	tokens = _splitBuffer(buffer, " ");
-	if (tokens.size() < 1)
-	{
-		_sendResponse(fd, ERR_MISSINGPARAMS(_hostname, client->getNname()));
-		return;
-	}
+        std::string prefix = channel->isChannelOperator(c->getNname()) ? "@" : "";
+        std::string reply = ":" + _hostname + " 352 " + client->getNname() + " " +
+                            params + " " + c->getUname() + " " + c->getHostName() +
+                            " " + _hostname + " " + c->getNname() + " H" + prefix +
+                            " :0 " + c->getRealName() + CRLF;
 
-	std::string	channelName = tokens[0];
-	Channel		*channel = _getChannel(channelName);
-
-	if (!channel)
-	{
-		_sendResponse(fd, ERR_NOSUCHCHANNEL2(_hostname, client->getNname(), channelName));
-		return;
-	}
- 	for (size_t i = 0; i < channel->getChClients().size(); i++)
-	{
-		Client		*c = channel->getChClients()[i];
-		std::string	prefix = channel->isChannelOperator(c->getNname()) ? "@" : "";
-		std::string	reply =
-			":" + _hostname + " 352 " + client->getNname() + " " +
-			channelName + " " + c->getUname() + " " + c->getHostName() +
-			" " + _hostname + " " + c->getNname() + " H" + prefix + " :0 " +
-			c->getRealName() + CRLF;
-		_sendResponse(fd, reply);
-	}
-
-	std::string	endReply =
-		":" + _hostname + " 315 " + client->getNname() +
-		" " + channelName + " :End of WHO list" + CRLF;
-	_sendResponse(fd, endReply);
+        _sendResponse(fd, reply);
+    }
+    std::string endReply = ":" + _hostname + " 315 " + client->getNname() + " " +
+                           params + " :End of WHO list" + CRLF;
+    _sendResponse(fd, endReply);
 }
